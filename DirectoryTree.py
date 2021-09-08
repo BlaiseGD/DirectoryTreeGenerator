e883dd2a9dc4f2cd872203e4a72ad573c59e14ca
BlaiseGD <58518531+BlaiseGD@users.noreply.github.com>
2021-09-07 20:47:46 -0500

    Finds Files in Directory
    This is bugged as it shows the files for one directory on every directory. I don't know how to make this work honestly. Hard project.

--- DirectoryTree.py
+++ DirectoryTree.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 import pathlib
-import platform 
+import platform
 
 #(Root Directory) Bright Green \u001b[32;1m
 #(Current Directory unless root dir) Yellow \u001b[33m
@@ -11,17 +11,23 @@ import platform
 #(Reset Colors) \u001b[0m
 
 directorytab = '';
-plt = platform.system()
-ogdirectory = str(pathlib.Path().resolve())
-print(ogdirectory);
+plt = platform.system(); #Determines what os you're on to use the correct directory seperator
+ogdirectory = str(pathlib.Path().resolve());
+ogDirIter = ogdirectory; 
+directoryFiles = os.listdir(ogdirectory);
 
 if plt == "Windows":
     splitDirectory = ogdirectory.split('\\');
 else:
     splitDirectory = ogdirectory.split('/');
 
+splitDirectory = splitDirectory[1:]; #Takes off the blank directory aka first '/'
+count = 0;
+modOgDir = ogdirectory.rindex('/');
 for i in splitDirectory:
-    if os.path.isfile(ogdirectory) == False:
-       print(directorytab,'--|\u001b[34;1m', i , '\u001b[0m')
+    if os.path.isdir(ogdirectory) == True:
+       print(directorytab,'\u001b[34;1m--|', i , '\u001b[0m');
        directorytab += '    ';
-       
+       for j in range(len(directoryFiles)):
+           print(directorytab,'|-',directoryFiles[j]);
+#NO CLUE WHAT TO DO RN
